60aeee3c31d635ed1ee85e9fa8316866902c7382
rupalikarande <34995222+rupalikarande@users.noreply.github.com>
2018-02-19 14:05:15 +0530

    updated files

--- form.php
+++ form.php
@@ -8,24 +8,124 @@
      <link rel="stylesheet" href="main.css">
     </head>
     <body>
-            
+    <!-- database connection -->
+    <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dummy";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//  if(isset($_POST['add']))
+// {
+// add data
+$name_a="";
+$phone="";
+$Add="";
+$city="";
+$email="";
+
+  $name_a=$_POST['name'];
+  $phone=$_POST['Phone_No'];
+  $Add=$_POST['Address'];
+  $city=$_POST['City'];
+  $email=$_POST['Email'];
+ // prepare and bind
+  $stmt = $conn->prepare("INSERT INTO textdemo (name,Phone_No,Address,City,Email) VALUES (?, ?,?,?,?)");
+  $stmt->bind_param("sssss", $name_a,$phone,$Add,$city,$email);
+  // set parameters and execute
+   $stmt->execute();
+   
+   $result = mysqli_query($conn,$stmt);
+        if ($result) {
+        echo "<script type='text/javascript'>alert('submitted successfully!')</script>";
+    }
+    else
+    {
+        echo "<script type='text/javascript'>alert('failed!')</script>";
+    }
+
+  $stmt->close();
+  
+  // }
+  
+  
+mysqli_close($conn);
+
+?>
+<!-- validation -->
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $phoneErr= "";
+$name = $Email = $Phone_No = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["Email"])) {
+    $emailErr = "Email is required";
+  } else {
+   $Email = test_input($_POST["Email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+
+   if(empty($_POST["Phone_No"])){
+    $cleared = preg_replace('/^[0-9]/', '', $value);
+    if (count($cleared)<10 || count($cleared)>14) {
+      echo 'Please enter a valid phone number';
+  }else{
+      echo "valid";
+  }
+    }
+   
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+  ?>    
+  <h2 style="text-align:center;color:blue;font-family:Times New Roman, Times, serif;"><b>Customer Visit Form</b></h2>   
         <div   style="margin-left:50px; margin-top:20px; padding:50px; border: 1px solid black; margin-right:50px;"> 
-        <h2 style="text-align:center;color:blue;font-family:Times New Roman, Times, serif;"><b>Login Form</b></h2>
-        <div class="container" >
+        <p><span class="error">* required field.</span></p>
+        <!-- <div class="container" > -->
                  <div class="row">
               <div class="col-sm-8">
-                <form action="server.php" method="post">
-                   <input type="text" id="name" name="name" placeholder=" Name: "><br><br>
-                   <input type="text" id="Phone_No" name="Phone_No" placeholder=" Phone No: "><br><br>
+                <form action="" method="post">
+                   <b><input type="text" id="name" name="name" placeholder=" Name: ">
+                   <span class="error">* <?php echo $nameErr;?></span><br><br>
+                   <input type="text" id="Phone_No" name="Phone_No" placeholder=" Phone No: ">
+                   <span class="error">* <?php echo $phoneErr;?></span><br><br>
                    <input type="text" id="Address" name="Address" placeholder=" Address: "><br><br>
                    <input type="text" id="City" name="City" placeholder=" City: "><br><br>
-                    <input type="text" id="Email" name="Email" placeholder=" Email: "><br><br>
+                    <input type="text" id="Email" name="Email" placeholder=" Email: ">
+                    <span class="error">* <?php echo $emailErr;?></span><br><br>
                    <i > <input type="submit" value="add"></i>
-                    <i><input type="submit" value="Cancel"></i>
+                    <!-- <i><input type="submit" value="Cancel"></i> -->
+                  <i> <input type="reset" value="Cancel"><i>
                     <i><input type="submit" value="Message"></i>
              </div>
              <div class="col-sm-4">
-                   <div>                   
+                                    
                    <input type="submit" value="View Database"><br><br>
                    <input type="submit" value="Send Bulk-Message"><br><br>
                    <input type="submit" value="Display Profile"><br><br>
@@ -33,6 +133,16 @@
               </div>
             </div>
           </div>
-        </div>
+        <!-- </div> -->
+        <?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $Email;
+echo "<br>";
+echo $Phone_No;
+echo "<br>";
+
+?>
     </body>
 </html>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+    <head>
+        <title>hotels login </title>
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css?famiy=Open+Sans+Condensed:300" rel="stylesheet" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel="stylsheet" type="text/css">
+    </head>
+    <body>
+            <?php
+            session_start();
+            $con=mysqli_connect("localhost","root","","dummy");
+            if(isset($_POST['login']))
+               {
+               if($con)
+               {
+            $user_name=$_POST['username'];
+            $pwd=$_POST['password'];
+            $query="SELECT * FROM session WHERE username='".$user_name."' and password='".$pwd."' ";
+            $result=mysqli_query($con,$query);
+            if(mysqli_num_rows($result)>0)
+               {
+            while($row=mysqli_fetch_assoc($result))
+               {
+            session_start();
+            $_SESSION['user_name']=$row['username'];
+            $_SESSION['user_role']=$row['role'];
+            header('location:form.php');
+               }
+               }
+            else
+              {
+            echo "<script>alert('invalid login')</script>";
+              }
+            }
+            }
+            ?>
+    
+            <div id="logo">
+                    <img src="Hotelscom.png" width:"118px"; height:"38px" >
+                </div>
+            
+                <div class="base">
+                 
+            
+             <div id="form1">
+                 <div id="form-img">
+                  <img src="profile.png" width="99" height="99">
+                 </div>
+                   <div id="mailbox">
+                        <form  action="form1.php" method="post">
+
+                       <input placeholder="Enter your id" type="text" name="name" style="width:270px; height:42px; border: solid 1px #c4c2c6; font-size:16px; padding-left:8px;"><br><br>   
+                       <input placeholder="Enter your password" type="password" name="name" style="width:270px; height:42px; border: solid 1px #c4c2c6; font-size:16px; padding-left:8px;"> <br><br>
+                       <input type="submit" id="button2" value="Submit">
+                       </form>
+                    </div>
+                </div>
+             <div id="innfo4">
+                 <a href="#"/>Register Here</a>
+             </div><br>
+            
+             <div>
+                 <img src="hotellogo.png" id="logo2"/>
+                 
+             </div>
+            
+            </div>       
+    
+    </body>
+</html>
